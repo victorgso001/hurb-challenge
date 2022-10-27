9b8bb4cf79b9b456bb5495b3d6c55b571a329963
Victor Oliveira <victor.gso001@gmail.com>
2022-10-27 12:04:55 -0300

    Changed controllers folder and app run port

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, Response, request
 
 
-from model.fit import fit
-from model.predict import predict
+from controllers.fit import fit
+from controllers.predict import predict
 
 app = Flask(__name__)
 
@@ -19,4 +19,4 @@ def predict_model():
     return predict(request)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(debug=True, port=6000, host='0.0.0.0')
